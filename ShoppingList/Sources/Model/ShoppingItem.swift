7f37e203350c62039d3f8725724676f71141dab1
KEEN <homurahomu@naver.com>
2021-10-14 14:53:21 +0900

    Model 생성

---
+++ ShoppingList/Sources/Model/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  ShoppingList
+//
+//  Created by KEEN on 2021/10/14.
+//
+
+import Foundation
+
+struct ShoppingItem {
+  var name: String
+  var isChecked: Bool = false
+  var isStared: Bool = false
+}
